a62e4f13ee3fc464e55854cebeabfb8c6a694266
zhanghongwei <zhanghongwei@juhuachunyinxiang.local>
2014-10-17 14:17:21 +0800

    Initial Commit

---
+++ SinaMicroblog/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SinaMicroblog
+//
+//  Created by zhanghongwei on 14-10-17.
+//  Copyright (c) 2014年 Manito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
